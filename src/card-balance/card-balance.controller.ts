9b037884dc764cd457cdc2cd2d55555ce9213192
Louis Alary <louis.alary@epsi.fr>
2025-04-14 12:34:56 +0200

    [FEAT] - Add prisma to project

--- src/card-balance/card-balance.controller.ts
+++ src/card-balance/card-balance.controller.ts
@@ -1,19 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CardBalanceService } from './card-balance.service';
 import { CheckCardBalanceDto } from './dto/check-card-balance.dto';
 import { CardResult } from './models/card-result.model';
+import { CardsService } from '@/card.service';
 
 @ApiTags('Card Balance')
 @Controller('card-balance')
 export class CardBalanceController {
-  constructor(private readonly cardBalanceService: CardBalanceService) {}
+  constructor(
+    private readonly cardBalanceService: CardBalanceService,
+    private readonly cardService: CardsService
+  ) {}
 
   /**
    * Check Carrefour gift card balance
    */
   @Post()
-  checkBalance(@Body() dto: CheckCardBalanceDto): Promise<CardResult | null> {
-    return this.cardBalanceService.checkCardBalance(dto);
+  async checkBalance(@Body() dto: CheckCardBalanceDto): Promise<CardResult | null> {
+    const data = await this.cardBalanceService.checkCardBalance(dto); // ✅ Attendre la réponse
+
+    if (data) {
+      await this.cardService.createCard({ 
+        cardNumber: dto.cardNumber,  // ✅ Prend la valeur du dto
+        pinCode: dto.pin,        // ✅ Prend la valeur du dto
+        balance: data.balance,       // ✅ Prend la balance retournée
+        validityDate: data.validityDate // ✅ Prend la validité retournée
+      });
+    }
+  
+    return data; // ✅ Retourner la réponse
   }
+
+   /**
+   * Get all cards from the database
+   */
+   @Get()
+   getAllCards(): Promise<CardResult[]> {
+     return this.cardService.cards({});
+   }
+
 }
